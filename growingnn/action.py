50c7a94574070c0a39b7d59680f417cc036ed8be
PjuralSzymon <59897259+PjuralSzymon@users.noreply.github.com>
2024-12-02 23:06:18 +0100

    feat() Adding empty action

--- growingnn/action.py
+++ growingnn/action.py
@@ -19,14 +19,15 @@ class Action:
         adding_layer_res_actions = Add_Res_Layer.generate_all_actions(Model)
         adding_layer_conv_seq_actions = Add_Seq_Conv_Layer.generate_all_actions(Model)
         adding_layer_conv_res_actions = Add_Res_Conv_Layer.generate_all_actions(Model)
-        #adding_layer_seq_output_action = Add_seq_output_layer.generate_all_actions(Model)
         delete_layer_actions = Del_Layer.generate_all_actions(Model)
-        #result += adding_layer_seq_output_action
+        empty_action = Empty.generate_all_actions(Model)
+        
         result += adding_layer_seq_actions
         result += adding_layer_res_actions
         result += adding_layer_conv_seq_actions
         result += adding_layer_conv_res_actions
         result += delete_layer_actions
+        result += empty_action
         return result
 
 class Add_Seq_Layer(Action):
@@ -51,18 +52,18 @@ class Add_Seq_Layer(Action):
     def __str__(self):
         return " ( Add Seq Layer Action: " + str(self.params) + " ) "
 
-class Add_seq_output_layer(Action):
+class Empty(Action):
     def execute(self, Model):
-        Model.add_sequential_output_Layer()
+        pass
 
     def can_be_infulenced(self, by_action):
         return False
 
     def generate_all_actions(Model):
-        return [Add_seq_output_layer(None)]
+        return [Empty(None)]
 
     def __str__(self):
-        return " ( Add sequential output Action )"
+        return " ( Empty Action )"
     
 class Add_Res_Layer(Action):
     def execute(self, Model):
@@ -81,12 +82,12 @@ class Add_Res_Layer(Action):
         for pair in pairs:
             if type(Model.get_layer(pair[1])) != Conv:
                 actions.append(Add_Res_Layer([pair[0], pair[1], Layer_Type.ZERO]))
-        for pair in pairs:
-            if type(Model.get_layer(pair[1])) != Conv:
-                actions.append(Add_Res_Layer([pair[0], pair[1], Layer_Type.RANDOM]))
-        for pair in pairs:
-            if type(Model.get_layer(pair[1])) != Conv:
-                actions.append(Add_Res_Layer([pair[0], pair[1], Layer_Type.EYE]))
+        # for pair in pairs:
+        #     if type(Model.get_layer(pair[1])) != Conv:
+        #         actions.append(Add_Res_Layer([pair[0], pair[1], Layer_Type.RANDOM]))
+        # for pair in pairs:
+        #     if type(Model.get_layer(pair[1])) != Conv:
+        #         actions.append(Add_Res_Layer([pair[0], pair[1], Layer_Type.EYE]))
         return actions
     
     def __str__(self):
