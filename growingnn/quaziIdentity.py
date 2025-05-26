1fdc1621e01f29733483e7d7309270cd6554ec18
PjuralSzymon <59897259+PjuralSzymon@users.noreply.github.com>
2025-05-26 21:39:36 +0200

    refactor(R4) Moving Reshape to numba

--- growingnn/quaziIdentity.py
+++ growingnn/quaziIdentity.py
@@ -12,7 +12,7 @@ def eye_stretch(a, b):
     if a == b:
         return np.eye(a)
     A = np.eye(max(a, b))
-    return np.ascontiguousarray(cv.resize(A, (a, b)).T)
+    return cv.resize(A, (a, b)).T
 
 def get_reshsper(size_from, size_to):
     if size_from == size_to:
@@ -22,11 +22,10 @@ def get_reshsper(size_from, size_to):
         RESHEPERS[key] = eye_stretch(size_from, size_to)
     return RESHEPERS[key]
 
+@jit(nopython=True)
 def Reshape(x, output_size, QIdentity):
     if QIdentity is None:
         return x[:output_size, :]
-    
-    # Pre-allocate only if we need QIdentity
     x_reshaped = np.empty((output_size, x.shape[1]))
     for i in range(x.shape[1]):
         x_reshaped[:, i] = np.dot(x[:, i], QIdentity)
